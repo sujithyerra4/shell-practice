561aa69e01447062795f49f69ca956c7e946d1b8
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 13:32:22 +0530

    redirectors

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -17,11 +17,11 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
- echo -e $R please proceed with root privilages $N | tee -a LOG_FILE
+ echo -e $R please proceed with root privilages $N | tee -a $LOG_FILE
 fi
 USAGE(){
 
-    echo -e " $R USAGE $N :sudo sh.14-usage.sh package1  package2 package3...." | tee -a LOG_FILE
+    echo -e " $R USAGE $N :sudo sh.14-usage.sh package1  package2 package3...." | tee -a $LOG_FILE
     exit 1
 }
 
@@ -30,30 +30,30 @@ then
  USAGE
  fi
 
-echo "script started executing at : $(date)" | tee -a LOG_FILE
+echo "script started executing at : $(date)" | tee -a $LOG_FILE
 
 VALIDATE(){
 
   if [ $1 -ne 0 ]
    then
-   echo -e "$2 is $R failure $N" | tee -a LOG_FILE
+   echo -e "$2 is $R failure $N" | tee -a $LOG_FILE
    exit1
    else
-   echo -e "$2 is $G success $N" | tee -a LOG_FILE
+   echo -e "$2 is $G success $N" | tee -a $LOG_FILE
    fi
 }
 
 for package in $@
 do 
-    dnf list installed $package &>> LOG_FILE
+    dnf list installed $package &>> $LOG_FILE
 
 if [ $? -ne 0 ]
 then
-  echo "$package  is not installed, going to install it" | tee -a LOG_FILE
-   dnf install $package  -y  &>> LOG_FILE
+  echo "$package  is not installed, going to install it" | tee -a $LOG_FILE
+   dnf install $package  -y  &>> $LOG_FILE
    VALIDATE $? "Installing  $package "
    else
-    echo -e "$package  is already $Y installed $N" | tee -a LOG_FILE
+    echo -e "$package  is already $Y installed $N" | tee -a $LOG_FILE
    fi
 
 done
