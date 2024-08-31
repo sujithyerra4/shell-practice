6cd79231e83ac80057543468856568103ba0714d
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 22:30:17 +0530

    mysql

--- 19-disk.sh
+++ 19-disk.sh
@@ -6,8 +6,8 @@ THRESHOLD=5
 
 while IFS= read -r line
 do 
-  USAUGE= echo $line|awk -F " " '{print $6F}'|cut -d "%" -f1
-  PARTITON= echo $line|awk -F " " '{print $NF}'
+  USAUGE= $(echo $line|awk -F " " '{print $6F}'|cut -d "%" -f1)
+  PARTITON= $(echo $line|awk -F " " '{print $NF}')
 
   if [ $USAUGE -ge $THRESHOLD ]
   then
