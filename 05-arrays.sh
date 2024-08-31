05c126f761d62acf389a53c4a9e7dd24d101f63e
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 07:24:47 +0530

    variables

---
+++ 05-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS={"APPLE","BANANA","ORANGE","GRAPES"}
+
+echo "1st fruit : ${FRUITS[0]}"
+echo "2st fruit : ${FRUITS[1]}"
+echo "3st fruit : ${FRUITS[2]}"
+echo "4st fruit : ${FRUITS[3]}"
+echo "all fruits : ${FRUITS[@]}"
+
