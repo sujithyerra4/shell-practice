390b3a35647b730d12089e6dc59520350df2b05c
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 08:53:20 +0530

    colors

--- 10-installation.sh
+++ 10-installation.sh
@@ -39,11 +39,11 @@ dnf list installed git
 else
  echo "git is already installed"
 
- if [ $? -eq 0 ]
- then 
-    dnf remove git 
-    echo " git is uninstalled"
-fi 
+#  if [ $? -eq 0 ]
+#  then 
+#     dnf remove git 
+#     echo " git is uninstalled"
+# fi 
 
  fi
 
---
+++ 14-usage.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USAGE(){
+
+    echo "sh.14-usage.sh git nginx mysql...."
+}
+
+USAGE
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+echo "please proceed with root privilages"
+exit 1
+fi
+
+VALIDATE(){
+
+  if [ $1 -ne 0 ]
+   then
+   echo -e "$2 is $R failure $N"
+   exit1
+   else
+   echo -e "$2 is $G success $N"
+   fi
+}
+
+for package in $@
+do 
+    dnf list installed $package 
+
+if [ $? -ne 0 ]
+then
+  echo "$package  is not installed, going to install it" 
+   dnf install $package  -y
+   VALIDATE $? "Installing  $package "
+   else
+    echo -e "$package  is already $Y installed $N"
+   fi
+
+done
