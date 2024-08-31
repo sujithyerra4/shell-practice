32c05f2eafc603d916d9cc3d1a06747d6e96b19a
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 07:21:03 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Sujith
 PERSON2=hruthik
 
-echo " $PERSON1:hii $PERSON2 ,how are you?"
+echo "$PERSON1:hii $PERSON2 ,how are you?"
 echo "$PERSON2:Hii $PERSON1 , i am fine,what about you?"
 echo "$PERSON1:i am fine,what you doing these days"
 echo "$PERSON2:i am learning shell scripting"
--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1:hii $PERSON2 ,how are you?"
-echo "$:Hii $PERSON1 , i am fine,what about you?"
+echo "$PERSON2:Hii $PERSON1 , i am fine,what about you?"
 echo "$PERSON1:i am fine,what you doing these days"
 echo "$PERSON2:i am learning shell scripting"
