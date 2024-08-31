76d9ef6ffb87ae4d1daea6786e3687768cb2906d
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 22:22:50 +0530

    mysql

---
+++ 19-disk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK=df -hT|grep xfs
+THRESHOLD=5
+
+
+
+
+while IFS= read -r line
+do 
+  USAUGE= echo $line|grep xfs|awk -F " " '{print $6F}'|cut -d "%" -f1
+  PARTITON= echo $line|grep xfs|awk -F " " '{print $NF}'
+  if [ $USAGE -gt $THRESHOLD]
+  echo $PARTITON is greater than $THRESHOLD : current value :$USAGE , please check
+  fi
+done <<< $DISK
