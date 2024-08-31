9cea3dd87e547ba1bde34fd821afd3fc08617cc9
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 22:32:16 +0530

    mysql

--- 19-disk.sh
+++ 19-disk.sh
@@ -6,10 +6,13 @@ THRESHOLD=5
 
 while IFS= read -r line
 do 
-  USAUGE= $(echo $line|awk -F " " '{print $6F}'|cut -d "%" -f1)
-  PARTITON= $(echo $line|awk -F " " '{print $NF}')
+#   USAUGE= $(echo $line|awk -F " " '{print $6F}'|cut -d "%" -f1)
+#   PARTITON= $(echo $line|awk -F " " '{print $NF}')
 
-  if [ $USAUGE -ge $THRESHOLD ]
+USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+
+  if [ $USAGE -ge $THRESHOLD ]
   then
   echo $PARTITON is greater than $THRESHOLD : current value :$USAGE , please check
   fi
