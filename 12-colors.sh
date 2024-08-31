934e84b8947cad545ce4c2150371af3997d26ea0
sujithyerra4 <sujithyerra483@gmail.com>
2024-08-31 08:29:45 +0530

    colors

---
+++ 12-colors.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+echo "please proceed with root privilages"
+exit 1
+fi
+
+VALIDATE(){
+
+  if [ $1 -ne 0 ]
+   then
+   echo -e "$2  installion is $Rfailure$N"
+   else
+   echo "$2 installion is $Gsuccess$N"
+   fi
+
+}
+
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+  echo "git is not installed, going to install it" 
+   dnf install git -y
+   VALIDATE $? git
+   else
+    echo -e "git is already $Yinstalled$N"
+   fi
+
+   
+ dnf list installed nginx
+
+ if [ $? -ne 0 ]
+ then 
+  echo "Nginx is not installed, going to install it"
+     dnf install nginx
+   VALIDATE $? nginx
+else
+ echo -e "Nginx is already $Yinstalled$N"
+ fi
